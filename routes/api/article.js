3573cd300f072760c5dd89b0e6b5759f1ead6b1a
Nadia-Kadri <nadiagrace96@gmail.com>
2020-04-04 19:06:38 -0400

    scrape articles into database

--- routes/api/article.js
+++ routes/api/article.js
@@ -1,21 +1,37 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../models");
+// Scraping dependencies
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-// Post route for creating an article
-router.post("/create", function(req, res) {
-  console.log("creating note");
+// Scrape data from WSJ and place it into Articles collection within MongoScraper db
+router.get("/scrape", function(req, res) {
+  axios.get("https://www.wsj.com/").then(function(res) {
+    // Load the html body from axios into cheerio
+    const $ = cheerio.load(res.data);
+      
+    $("article").each(function(i, element) {
+      let title = $(element).find("h3").text();
+      let summary = $(element).find("p").text();
+      let link = $(element).find("a").attr("href");
 
-  db.Article.create({ 
-    title: req.body.title, 
-    summary: req.body.summary, 
-    link: req.body.link
-   })
-  .then(result => {
-    console.log("article created")
-    res.send(result)
+      if (title && summary && link) {
+        // Insert the data in MongoScraper db
+        db.Article.create({
+          title: title, 
+          summary: summary, 
+          link: link
+        })
+        .then(result => {
+          console.log("Article inserted into database")
+        })
+        .catch(err => console.log(err.message));
+      }
+    });
   })
-  .catch(err => console.log(err.message));
+  // Send a message to the client
+  res.send("Scrape Complete");
 });
 
 module.exports = router;
