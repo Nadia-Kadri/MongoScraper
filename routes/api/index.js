d7e26e56d27c6e1d96c8efb6349c197811b5c56c
Nadia-Kadri <nadiagrace96@gmail.com>
2020-04-04 13:23:23 -0400

    routes and models folder structure

---
+++ routes/api/index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const userRoutes = require("./user");
+const articleRoutes = require("./article");
+const noteRoutes = require("./note");
+
+// User routes
+router.use("/user", userRoutes);
+router.use("/article", articleRoutes);
+router.use("/note", noteRoutes);
+
+module.exports = router;
